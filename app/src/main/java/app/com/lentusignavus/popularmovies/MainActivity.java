178abb32157d4383e27f88e98a4a05a7c6c40a7a
lentusignavus <ignavus.ii@gmail.com>
2015-12-29 22:47:27 -0600

    Initial commit of Project 1

---
+++ app/src/main/java/app/com/lentusignavus/popularmovies/MainActivity.java
@@ -0,0 +1,23 @@
+package app.com.lentusignavus.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void showToast(View v) {
+        String stuff = (String) v.getContentDescription();
+        Toast.makeText(getApplication(), stuff, Toast.LENGTH_SHORT).show();
+    }
+}
