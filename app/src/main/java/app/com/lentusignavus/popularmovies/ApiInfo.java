aaa0eabc5b6fa88f60dd2c7f1a88721f4b36ef5d
lentusignavus <ignavus.ii@gmail.com>
2016-01-14 15:05:43 -0600

    Include api class so it doesn't crash on build

---
+++ app/src/main/java/app/com/lentusignavus/popularmovies/ApiInfo.java
@@ -0,0 +1,16 @@
+package app.com.lentusignavus.popularmovies;
+
+/**
+ * Created by kare2436 on 12/30/15.
+ */
+public class ApiInfo {
+    public static String getMoviedbKey() {
+        return "<insert API key here>";
+    }
+    public static String getImageBaseUrl(){
+        return "http://image.tmdb.org/t/p/";
+    }
+    public static String getApiBaseUrl() {
+        return "http://api.themoviedb.org/3";
+    }
+}
